b6238e1393dfb3fde80ffe0379b0f6b3c17d93d7
Leon Hsieh <hsiehce@gmail.com>
2021-11-24 16:48:39 +0800

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by leon on 2021/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
